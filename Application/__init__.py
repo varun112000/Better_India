27ec1618df85ce16cb9830233d4cd2f5f68f8a25
Varun Palla <varunpalla99@gmail.com>
2020-12-19 14:10:41 +0530

    Adding login and register features

--- Application/__init__.py
+++ Application/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+cluster = MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false")
+db = cluster["BetterIndia"]
+users = db["Users"]
+issues = db["Issues"]
+
 
 from Application import routes
--- Application/routes.py
+++ Application/routes.py
@@ -2,16 +2,57 @@ from Application import app
 from flask import render_template, request, url_for, flash, redirect, session
 from werkzeug.security import generate_password_hash, check_password_hash
 from bson.objectid import ObjectId
+from Application import users, issues
 
 @app.route('/')
-@app.route('/Home')
+@app.route('/home')
 def index():
     return render_template('index.html')
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
+    if session.get('name'):
+        flash("Already Logged In", "info")
+        return redirect('/')
+
+    if request.method == "POST":
+        if users.find_one({'email':request.form['email']}):
+            if check_password_hash(users.find_one({'email':request.form['email']})['password'], request.form['password']):
+                # flash("Logged In","success")
+                session['name'] = users.find_one({'email':request.form['email']})['name']
+                session['email'] = request.form['email']
+                return redirect("/")
+            else:
+                # flash("Incorrect Password", "danger")
+                return redirect("/login")
+        else:
+            # flash("Email Not Found, Register Here", "danger")
+            return redirect("/register")
     return render_template('login.html')    
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
+    if session.get('name'):
+        flash("Already Registered", "info")
+        return redirect('/')
+
+    if request.method == "POST":
+        if users.find_one({'email':request.form['email']}):
+            flash("Email Already Exist's","danger")
+            return redirect("/register")
+
+        else:    
+            password = generate_password_hash(request.form['password'])
+            users.insert_one({'name':request.form['name'], 'phone_number':request.form['phonenumber'], 'email':request.form['email'], 'password':password})
+            flash("You are Successfully Registered","success")
+            return redirect("/login")
+
     return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('name',None)
+    session.pop('email',None)
+    flash("Successfully logged out",'success')
+    return redirect('/')
+    
