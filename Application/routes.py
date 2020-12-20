0e201238434e377f3467d3735341fb5b789cdb7d
Varun Palla <varunpalla99@gmail.com>
2020-12-20 11:51:05 +0530

    Adding status update features for admin, Status Update and Falsh features

--- Application/routes.py
+++ Application/routes.py
@@ -25,15 +25,14 @@ def login():
     if request.method == "POST":
         if users.find_one({'email':request.form['email']}):
             if check_password_hash(users.find_one({'email':request.form['email']})['password'], request.form['password']):
-                # flash("Logged In","success")
                 session['name'] = users.find_one({'email':request.form['email']})['name']
                 session['email'] = request.form['email']
                 return redirect("/")
             else:
-                # flash("Incorrect Password", "danger")
+                flash("Incorrect Password", "danger")
                 return redirect("/login")
         else:
-            # flash("Email Not Found, Register Here", "danger")
+            flash("Email Not Found, Register Here", "danger")
             return redirect("/register")
     return render_template('login.html')    
 
@@ -60,77 +59,116 @@ def register():
 def logout():
     session.pop('name',None)
     session.pop('email',None)
-    flash("Successfully logged out",'success')
     return redirect('/')
     
 @app.route('/addissue', methods = ["GET", "POST"])
 def addissue():
-    if request.method == "POST":
-        if issues.find_one({'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email')}):
-            # flash("Email Already Exist's","danger")
-            return redirect("/issues")
+    if session.get('name'):
+        if request.method == "POST":
+            if issues.find_one({'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email')}):
+                flash("Issue Already Exist's","danger")
+                return redirect("/issues")
 
-        else:
-            status = ""
-            upvote = list()
-            messages = list()
-            # with grid_fs.new_file(filename = request.form['image']) as fp:
-            #     fp.write(request.data)
-            #     file_id = fp._id
-            # if grid_fs.find_one(file_id) is not None:
-            issues.insert_one({'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email'), 'status':status, 'upvote':upvote, 'messages':messages})
-            # flash("You are Successfully Registered","success")
-            return redirect("/issues")
-        
-    return render_template('addissue.html')    
+            else:
+                status = "Pending"
+                upvote = list()
+                messages = list()
+                # with grid_fs.new_file(filename = request.form['image']) as fp:
+                #     fp.write(request.data)
+                #     file_id = fp._id
+                # if grid_fs.find_one(file_id) is not None:
+                issues.insert_one({'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email'), 'status':status, 'upvote':upvote, 'messages':messages})
+                flash("Issue Added","success")
+                return redirect("/issues")
+        return render_template('addissue.html')
+    else:
+        return redirect('/login')    
     
 
 @app.route('/issues')
 def allissues():
-    infos = list(issues.find({}))
-    return render_template('issues.html', infos=infos)
+    if session.get('name'):
+        infos = list(issues.find({}))
+        return render_template('issues.html', infos=infos)
+    else:
+        return redirect('/login')
+
 
 @app.route('/editissue/<string:idx>', methods = ["GET", "POST"])
 def editissue(idx=None):
-    if request.method == 'POST':
-        issues.find_one_and_update({'_id':ObjectId(idx)},{"$set":{'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email')}})
-        return redirect('/issues')
+    if session.get('name'):
+        if request.method == 'POST':
+            issues.find_one_and_update({'_id':ObjectId(idx)},{"$set":{'title':request.form['title'], 'location':request.form['location'], 'date':request.form['date'], 'details':request.form['details'], 'name':session.get('name'), 'email':session.get('email')}})
+            flash("Issue Updated","success")
+
+            return redirect('/issues')
+        else:
+            data = issues.find({'_id':ObjectId(idx)})[0]
+            return render_template('editissue.html', data= data)
     else:
-        data = issues.find({'_id':ObjectId(idx)})[0]
-        return render_template('editissue.html', data= data)
+        return redirect('/login')
 
 @app.route('/deleteissue/<string:idx>')
 def deleteissue(idx=None):
-    issues.delete_one({'_id':ObjectId(idx)})
-    return redirect('/myissues')
+    if session.get('name'):
+        issues.delete_one({'_id':ObjectId(idx)})
+        flash("Issue Deleted","success")
+
+        return redirect('/myissues')
+    else:
+        return redirect('/login')
 
 @app.route('/myissues')
 def myissue():
-    infos = list(issues.find({'name':session.get('name'), 'email':session.get('email')}))
-    return render_template('myissues.html', infos=infos)
+    if session.get('name'):
+        infos = list(issues.find({'name':session.get('name'), 'email':session.get('email')}))
+        return render_template('myissues.html', infos=infos)
+    else:
+        return redirect('/login')
+
 
 @app.route('/issues/<string:idx>/messages')
 def messages(idx = None):
-    data = issues.find({'_id':ObjectId(idx)})[0]
-    a = dict()
-    a['id'] = idx
-    a['title'] = data['title']
-    a['messages'] = data['messages']
-    return render_template('message.html', mess = a )
+    if session.get('name'):
+        data = issues.find({'_id':ObjectId(idx)})[0]
+        a = dict()
+        a['id'] = idx
+        a['title'] = data['title']
+        a['messages'] = data['messages']
+        return render_template('message.html', mess = a )
+    else:
+        return redirect('/login')
 
 @app.route('/issues/<string:idx>/messages/add', methods=["POST", "GET"])
 def addmessage(idx=None):
-    if idx==None:
-        return redirect('/issues')
+    if session.get('name'):
+        if idx==None:
+            return redirect('/issues')
+        else:
+            b ={'name':session.get('name'), 'comment':request.form['comment']}
+            issues.update_one({'_id':ObjectId(idx)},{"$push":{'messages':b}},)
+            return redirect('/issues/'+str(idx)+'/messages')
     else:
-        b ={'name':session.get('name'), 'comment':request.form['comment']}
-        issues.update_one({'_id':ObjectId(idx)},{"$push":{'messages':b}},)
-        return redirect('/issues/'+str(idx)+'/messages')
-
+        return redirect('/login')
 
 @app.route('/upvote/<string:idx>')
 def upvote(idx=None):
-    b = {'name':session.get('name'), 'email':session.get('email')}
-    issues.update_one({'_id':ObjectId(idx)},{"$addToSet":{'upvote':b}})
-    return redirect('/issues')
+    if session.get('name'):
+        b = {'name':session.get('name'), 'email':session.get('email')}
+        issues.update_one({'_id':ObjectId(idx)},{"$addToSet":{'upvote':b}})
+        return redirect('/issues')
+    else:
+        return redirect('/login')
+
+@app.route('/issues/<string:idx>/updatestatus', methods = ["POST", "GET"])
+def status(idx=None):
+    if session.get('name'):
+        if request.method == "POST":
+            issues.update_one({'_id':ObjectId(idx)},{"$set":{'status':request.form['status']}})
+            print(issues.find({}))
+            return redirect('/issues')
+    else:
+        return redirect('/login')
+
+
 
